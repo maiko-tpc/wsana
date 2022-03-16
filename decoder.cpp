01c88767c40fa43a226d785ba509f22bd8ce3852
tamidaq <tamidaq>
2022-03-16 10:39:17 +0900

    camac inp

--- decoder.cpp
+++ decoder.cpp
@@ -486,7 +486,6 @@ int ana_inp(evtdata *evt, unsigned int *rawdata, unsigned int size,
     printf("camac inp=%d\n", data16);
     for(int i=0; i<N_INP; i++){
       evt->camac_inp[i] = (data16>>i)&0x1;
-      printf("inp ch%d=%d\n", evt->camac_inp[i]);
     }
   }
 

5a7faa8b2ee1900b7b5a654298fd69d97f7d3e44
furuno <furuno@rcnp.osaka-u.ac.jp>
2023-06-13 21:32:31 +0900

    Modify online.sh to use inode to detect a new file

--- online.sh
+++ online.sh
@@ -2,17 +2,18 @@
 
 while :
 do
-    lastfile1=`ls -tr data/*.bld | tail -n 1`
-    echo $lastfile1
+    lastfile1=(`ls -tri data/*.bld | tail -n 1`)
+    echo ${lastfile1[1]}
     
-    ./ana $lastfile1 online.root -online -last -web 5920 &
+    ./ana ${lastfile1[1]} online.root -online -last -web 5920 &
     pid=$!
     echo $pid
     
+# Compare inode to detect a new file
     while :
     do
-	lastfile2=`ls -tr data/*.bld | tail -n 1`    
-	if [ $lastfile2 = $lastfile1 ]; then
+	lastfile2=(`ls -tri data/*.bld | tail -n 1`)
+	if [ ${lastfile2[0]} = ${lastfile1[0]} ]; then
 	    sleep 1
 	else
 	    echo "New file detected."
